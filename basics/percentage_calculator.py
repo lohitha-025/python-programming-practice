ef464a214c6638d4615cbf31902e38e215d44e79
Lohitha <sailohitha2006@gmail.com>
2026-08-29 20:41:45 +0530

    Add percentage calculator program

---
+++ basics/percentage_calculator.py
@@ -0,0 +1,6 @@
+total_marks = float(input("Enter total marks: "))
+obtained_marks = float(input("Enter obtained marks: "))
+
+percentage = (obtained_marks / total_marks) * 100
+
+print("Percentage:", percentage, "%")
